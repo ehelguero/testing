dcb23592ee074a71747d39b9e5c6eb4b643a1901
loupgarou81 <leloupgarou81@gmail.com>
2016-07-10 00:49:11 +0100

    Added test for sum items in CartSummary

--- test/cart-summary-test.js
+++ test/cart-summary-test.js
@@ -6,5 +6,23 @@ describe('CartSummary', function(){
   it('getSubtotal() should return 0 if no items are passed in', function() {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubtotal()).to.equal(0);
+  });
+
+  it('getSubtotal() should return the sum of the price and quantity for all items', function(){
+    //pasar objetos
+    var cartSummary = new CartSummary([
+      {
+        name: 'box',
+        price: 10,
+        quantity: 1,
+      },
+      {
+        name: 'tshirt',
+        price: 20,
+        quantity: 2,
+      }
+    ]);
+    // que devuelva el total
+    expect(cartSummary.getSubtotal()).to.equal(50);
   })
 })
